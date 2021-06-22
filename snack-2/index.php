f323e5cd1270cf7140c5428522ee6ac707ad7912
Cargalher <cargalher@gmail.com>
2021-06-22 16:22:46 +0200

    function  name longer 3 caracter

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,15 +1,25 @@
-<!-- Passare come parametri GET : name, mail e age e
-verificare (cercando i metodi che non conosciamo nella documentazione):
-- che name sia più lungo di 3 caratteri, 
-- che mail contenga un punto e una chiocciola e
-- che age sia un numero. 
-- Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+<!-- Passare come parametri GET : name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione):
+1. che name sia più lungo di 3 caratteri, 
+2. che mail contenga un punto e una chiocciola e
+3. che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php 
+
 var_dump($_GET);
 $_GET['name'];
-$_GET['mail'];
-$_GET['age'];
+// $_GET['mail'];
+// $_GET['age'];
+
+// name > 3 caracters
+$nameLonger = getName($name);
+$nameLonger = (strlen($name) > 3);
+
+
+
+function getName($writen_name){
+    return strlen($writen_name)>3;
+}
 
 ?>
 <!DOCTYPE html>
@@ -21,6 +31,16 @@ $_GET['age'];
     <title>Verify data</title>
 </head>
 <body>
+<p>
+ <?php 
+    if($nameLonger){
+        echo "Accesso riuscito";
+    }else{
+        echo "Accesso negato";
+    }
     
+    ?>
+</p>
+   
 </body>
 </html>
