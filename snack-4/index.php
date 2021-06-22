6484e53536c7d0ee9583193d922e4cda654d74ac
Cargalher <cargalher@gmail.com>
2021-06-22 17:28:21 +0200

    snack 5 done

--- snack-4/index.php
+++ snack-4/index.php
@@ -1,1 +1,9 @@
-<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<!-- Creare un array con 15 numeri casuali,
+ tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+ <?php
+$randomNumber = rand(1, 15);
+var_dump($randomNumber);
+
+
+ ?>
--- snack-5/index.php
+++ snack-5/index.php
@@ -1,1 +1,12 @@
-<!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+<!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi.
+ Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+
+ <?php 
+    $quote = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, voluptate. Cum aliquam minima sequi nesciunt nihil culpa alias expedita odio voluptatem dolor. hic natus perspiciatis repudiandae, ut illum esse sapiente nostrum itaque dolores. Temporibus tenetur repellat sit, magni quod. ducimus alias, exercitationem quisquam aut et iste doloribus qui quia! Obcaecati";
+    $quoteDivided = explode(".", $quote);
+    echo "<pre>";
+    var_dump($quoteDivided);
+    echo "</pre>";
+    
+ 
+ ?>
