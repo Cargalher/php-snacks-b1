43bfc54832cb324eaf13ce12ec7e11230d78f9f1
Cargalher <cargalher@gmail.com>
2021-06-22 14:58:56 +0200

    array first match created

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,2 +1,29 @@
-<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 -->
+<!-- - Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+- Stampiamo a schermo tutte le partite con questo schema:
+- Olimpia Milano - Cantù | 55-60  -->
+
+<?php 
+
+$matches = [
+    "home" => 'Chicago Bulls',
+    "visitor" => 'Los Angeles Lakers',
+    "homeScore" => '65',
+    "visitorScore" => '33',
+
+];
+var_dump($matches);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basketball Match</title>
+</head>
+<body>
+    
+</body>
+</html>
